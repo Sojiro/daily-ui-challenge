b6f2e61042a2052782af2a332e4fab9327b60361
Ashok M A <ashok5angel@gmail.com>
2019-08-12 20:54:44 +0200

    Finished the daily UI challenge 001

---
+++ 001-signup-form/src/app/sign-up-form/sign-up-form.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-sign-up-form',
+  templateUrl: './sign-up-form.component.html',
+  styleUrls: ['./sign-up-form.component.scss']
+})
+export class SignUpFormComponent implements OnInit {
+  signUpForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.signUpForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  signUp() {
+    this.signUpForm.valid && alert('Signed up successfully!');
+  }
+}
